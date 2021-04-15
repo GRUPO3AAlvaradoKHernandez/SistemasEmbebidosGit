df180e8433eefa5be72c2adcb683c192614c9e87
GRUPO3AAlvaradoKHernandez <katherine.hernandez.e@gmail.com>
2021-04-15 03:21:18 +0100

    Tarea 2update

--- Tarea2v2/Tarea22e3.c
+++ Tarea2v2/Tarea22e3.c
@@ -10,7 +10,9 @@ char cad[]="7EB3A50110140F3955025607EB7A50110140F39577301887EB3A50110140F3955025
 int k=0 ;
 
 point = strtok(cad, "E"); 
+
    while(point!=NULL){ //seguir mientras no termine el texto
+      if (k>0){//evita errores
          if (point[1] -'0' == 3){ /* es igual a tres imprime la temperatura*/
             printf("la temperatura es: %c",point[17]);
             printf("%c\n", point[18]);
@@ -20,6 +22,7 @@ point = strtok(cad, "E");
             printf("%c", point[18]);
             printf("%c\n", point[19]);
              }
+         }
          point = strtok(NULL,"E");
          k++; 
       }
